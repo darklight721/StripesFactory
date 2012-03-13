4016c8381fc7e55529b62b47c0c82f64e044c5ec
darklight721 <roysia2004@gmail.com>
2012-03-13 17:15:04 +0800

    Added rendering for angled orientation (actually partial). Todo: rendering for 0/90 deg and angled but the opposite direction.

--- stripes.js
+++ stripes.js
@@ -4,82 +4,84 @@
 	var ctx = null;
 
 	var StripesFactory = function() {
-		//stripe size, spacing, colors[], bgcolor, orientation
-		this.attr = {
-			"size"		: 0,
-			"spacing"	: 0,
-			"colors"	: [],
-			"bgColor"	: 0,
-			"orient"	: 0.0
-		};
+		
+		this.orient = 0.0;
+		this.stripes = [ /*{ size : 0, color : "" },{...},...*/ ];
 		
 		this.init = function() {
-			this.attr["size"] = 20;
-			this.attr["spacing"] = 15;
-			this.attr["colors"].push("#1F497D");
-			this.attr["bgColor"] = "#C6D9F1";
-			this.attr["orient"] = 0.0;
+			this.orient = 0.0;
+			this.stripes.push({ size : 20, color : "#1F497D" });
+			this.stripes.push({ size : 15, color : "#C6D9F1" });
 		};
 		
-		this.update = function(key,value) {
-			if (typeof this.attr[key] !== undefined)
+		this.setOrient = function(value) {
+			if (value !== undefined && typeof value === "number")
 			{
-				this.attr[key] = value;
+				this.orient = value;
 			}
 		};
 		
-		this.addColor = function(color) {
-			this.attr["colors"].push(color);
-		};
-		
-		this.removeColor = function(color) {
-			var colors = this.attr["colors"];
-			
-			if (color === undefined)
+		this.addStripe = function(value1,value2) {
+			if (value1 !== undefined && typeof value1 === "number" &&
+				value2 !== undefined && typeof value2 === "string")
 			{
-				colors.pop();
-			}
-			else
-			{
-				for (var i = 0; i < colors.length; i++)
-				{
-					if (colors[i] === color)
-					{
-						colors.splice(i,1);
-						break;
-					}
-				}
+				this.stripes.push({ size : value1, color : value2 });
 			}
 		};
 		
-		this.render = function() {
-			var width = (this.attr["colors"].length * this.attr["size"] + this.attr["spacing"]) * 2;
-			var height = width;
-			
-			// resize canvas
-			c.width = width/2;
-			c.height = height/2;
-			
-			ctx.translate(width/4,height/4);
-			ctx.rotate(this.attr["orient"]);
-			ctx.translate(0-(width/2),0-(height/2));
-			
+		this.removeStripe = function(index) {
+			if (index === undefined)
+			{
+				return this.stripes.pop();
+			}
+			else if (index >= 0 && index < this.stripes.length)
+			{
+				return this.stripes.splice(i,1);
+			}
+			return null;
+		};
+		
+		this.renderAngled = function() {
 			var that = this;
-			var x = 0;
 			
-			for (var i = 0; i < 2; i++)
-			{	
-				// draw stripes
-				$.each(this.attr["colors"],function(){
-					ctx.fillStyle = this.toString();
-					ctx.fillRect(x,0,that.attr["size"],height);
-					x += that.attr["size"];
-				});
-				
-				// draw space
-				ctx.fillStyle = this.attr["bgColor"];
-				ctx.fillRect(x,0,this.attr["spacing"],height);
-				x += this.attr["spacing"];
+			// compute canvas width by getting the width of the rotated stripes parallel to the x axis
+			// same thing for canvas height, parallel to y axis
+			var canvasWidth = 0;
+			var canvasHeight = 0;
+			$.each(this.stripes,function(){
+				var stripeWidth = Math.floor(this.size / Math.sin(that.orient));
+				canvasWidth += stripeWidth;
+				canvasHeight += Math.floor(stripeWidth * Math.tan(that.orient));
+			});
+			
+			// resize and clear canvas
+			c.width = canvasWidth;
+			c.height = canvasHeight;
+			
+			var multiplier = 2;
+			canvasWidth *= multiplier;
+			canvasHeight *= multiplier;
+			
+			// draw stripes, it's actually just a series of triangles
+			for (var i = 0; i < multiplier; i++)
+			{
+				for (var j = this.stripes.length-1; j >= 0; j--)
+				{	
+					ctx.fillStyle = this.stripes[j].color;
+					ctx.beginPath();
+					ctx.moveTo(0,0);
+					ctx.lineTo(canvasWidth,0);
+					ctx.lineTo(0,canvasHeight);
+					ctx.lineTo(0,0);
+					ctx.closePath();
+					ctx.fill();
+					
+					var stripeWidth = Math.floor(this.stripes[j].size / Math.sin(this.orient));
+					var stripeHeight = Math.floor(stripeWidth * Math.tan(this.orient));
+					
+					canvasWidth -= stripeWidth;
+					canvasHeight -= stripeHeight;
+				}
 			}
 			
 			return c.toDataURL();
@@ -87,14 +89,16 @@
 	};
 	
 	$(document).ready(function(){
-		//c = $("#c");
-		c = document.getElementById("c");
+		c = $("canvas")[0];
 		ctx = c.getContext("2d");
 	
 		var stripes = new StripesFactory();
 		stripes.init();
-		stripes.addColor("#4bacc6");
-		$("body").css("background-image","url('" + stripes.render() + "')");
+		stripes.setOrient(Math.PI/3);
+		var stripe = stripes.removeStripe();
+		stripes.addStripe(20,"#4bacc6");
+		stripes.addStripe(stripe.size,stripe.color);
+		$("body").css("background-image","url('" + stripes.renderAngled() + "')");
 	});
 
 })(jQuery);
